308e8b520d3a9bec0f34e46a369639b4759aea31
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-08-09 14:01:06 -0400

    initial dump

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+ * A Design by W3layouts
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+include_once("home.html");
+ ?>
